560378eae51d75c6cef5e213433b6ee502976f34
crdavis2 <collindavisr@gmail.com>
2019-03-25 19:57:56 -0500

    added packages

---
+++ src/PDFManipulator.java
@@ -0,0 +1,21 @@
+/*
+ * File: PDFManipulator.java
+ * Author: Collin Davis crdavis2@uab.edu
+ * Assignment:  EE333GroupProject - EE333 Spring 2019
+ * Vers: 1.0.0 03/24/2019 CRD - initial coding
+ *
+ * Credits:  (if any for sections of code)
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Collin Davis crdavis2@uab.edu
+ */
+public class PDFManipulator {
+
+}
