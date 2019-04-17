eab27d3ff745bd8f6fb9d9dc898999d07a3f1883
crdavis2 <collindavisr@gmail.com>
2019-04-17 14:51:40 -0500

    added module-info to repo

---
+++ src/module-info.java
@@ -0,0 +1,22 @@
+/*
+ * File: PDFToImage.java
+ * Author: Caleb Crocker   caleb98@uab.edu
+ * Author: Collin Davis    crdavis2@uab.edu
+ * Author: Anthony Lee     atlee974@uab.edu
+ * Author: Yasmin Sakalla  sakalyas@uab.edu
+ * Assignment:  EE333GroupProject - EE333 Spring 2019
+ * Vers: 1.0.0 03/24/2019 CRD - initial coding
+ *
+ */
+
+module EE333GroupProject {
+    requires commons.logging;
+    requires pdfbox;
+    requires pdfbox.tools;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;    
+    exports uab.group4.ee333;
+}
+
+
---
+++ src/uab/group4/ee333/PDFToImageFile.java
@@ -0,0 +1,22 @@
+/*
+ * File: PDFToImageFile.java
+ * Author: Collin Davis crdavis2@uab.edu
+ * Assignment:  EE333GroupProject - EE333 Spring 2019
+ * Vers: 1.0.0 04/16/2019 CRD - initial coding
+ *
+ * Credits:  (if any for sections of code)
+ */
+
+package uab.group4.ee333;
+import org.apache.pdfbox.tools.PDFToImage;
+
+/**
+ *
+ * @author Caleb Crocker   caleb98@uab.edu
+ * @author Collin Davis    crdavis2@uab.ed
+ * @author Anthony Lee     atlee974@uab.edu
+ * @author Yasmin Sakalla  sakalyas@uab.edu
+ */
+public class PDFToImageFile {
+
+}
